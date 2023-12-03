3f45906849d3385064f02de0aafc19075d0de809
Suman Kisku <sumankisku1@gmail.com>
2023-12-03 23:43:43 +0530

    added emojis

--- curseWords.js
+++ curseWords.js
@@ -1,7 +1,7 @@
 // List of curse words
 const curseWords = ["arse", "arsehead", "arsehole", "ass", "asshole", "bastard", "bitch", "bloody", "bollocks", "brotherfucker", "bugger", "bullshit", "child-fucker", "Christ on a cracker", "cock", "cocksucker", "crap", "cunt", "dick", "dickhead", "dyke", "fatherfucker", "frigger", "fuck", "goddamn", "godsdamn", "hell", "holy shit", "horseshit", "in shit", "Jesus fuck", "Jesus wept", "kike", "motherfucker", "nigga", "nigra", "pigfucker", "piss", "prick", "pussy", "shit", "shit ass", "shite", "sisterfucker", "slut", "son of a whore", "son of a bitch", "spastic", "turd", "twat", "wanker" /* Add more curse words here */];
 
-const emojiArray = ["❤️", "😄", "💘", /* Add more emojis here */];
+const emojiArray = ["❤️", "😄", "💘", "😊", "😍", "🥰", "😁", "🌈", "🌟", "🎉", "🤗" /* Add more emojis here */];
 
 module.exports = { curseWords, emojiArray }
 
